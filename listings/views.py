9d3b046a1ab84e475840d6c97c6daaf4b2ea6cc5
seydiv <sajadeydi8@gmail.com>
2020-10-21 20:17:50 +0330

    starting to make login page

--- listings/views.py
+++ listings/views.py
@@ -27,9 +27,45 @@ def listing(request,listing_id):
     return render(request,'listings/listing.html',context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    #keywords
+
+    if ('keywords' in request.GET) :
+        keywords = request.GET['keywords']
+        if (keywords):
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # city
+
+    if ('city' in request.GET) :
+        keywords = request.GET['city']
+        if (keywords):
+            queryset_list = queryset_list.filter(city__iexact=keywords)
+
+    #state
+    if ('state' in request.GET) :
+        state = request.GET['state']
+        if (state):
+            queryset_list = queryset_list.filter(state__icontains=state)
+
+    #bedrooms
+    if ('bedrooms' in request.GET) :
+        bedrooms = request.GET['bedrooms']
+        if (bedrooms):
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    #price
+    if ('price' in request.GET) :
+        price = request.GET['price']
+        if (price):
+            queryset_list = queryset_list.filter(price__lte=price)
+
     context = {
         'state_choices' : state_choices ,
         'bedroom_choices' : bedroom_choices ,
         'price_choices' : price_choices ,
+        'listings' : queryset_list , 
+        'values' : request.GET
     }
     return render(request,'listings/search.html',context)
